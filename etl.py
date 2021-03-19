610413f8c22aa0b0b87f637b2e61fda70f608903
Felipe Antonio Zadinello <felipezad@gmail.com>
2021-03-18 21:18:27 -0300

    Udacity Review

--- etl.py
+++ etl.py
@@ -7,37 +7,37 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, typ='series')
 
     # insert song record
-    song_data = 
+    song_data = list(df[["song_id", "title", "artist_id", "year", "duration"]].values)
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = list(df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].values)
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page']=='NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'],  unit='ms')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = (df['ts'], t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday)
+    column_labels = ['ts', 'hour', 'day', 'week', 'month', 'year', 'dayofweek']
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -45,7 +45,7 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+       
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
@@ -56,7 +56,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (pd.to_datetime(row.ts, unit='ms'), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
@@ -78,14 +78,13 @@ def process_data(cur, conn, filepath, func):
         conn.commit()
         print('{}/{} files processed.'.format(i, num_files))
 
-
 def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
-
+    conn.commit()
     conn.close()
 
 
--- sql_queries.py
+++ sql_queries.py
@@ -10,47 +10,95 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS users(user_id varchar PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar);
+CREATE TABLE IF NOT EXISTS users(
+        user_id int PRIMARY KEY,
+        first_name varchar not null, 
+        last_name varchar not null, 
+        gender varchar not null, 
+        level varchar not null)
+;
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS songs(song_id varchar  PRIMARY KEY, title varchar, artist_id varchar, year int, duration numeric);
+CREATE TABLE IF NOT EXISTS songs(
+            song_id varchar PRIMARY KEY,
+            title varchar not null, 
+            artist_id varchar not null, 
+            year int, 
+            duration float)
+            ;
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artists(artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude numeric, longitude numeric);
+CREATE TABLE IF NOT EXISTS artists(
+            artist_id varchar PRIMARY KEY,
+            name varchar not null, 
+            location varchar,
+            latitude numeric,
+            longitude numeric)
+            ;
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time(start_time varchar , hour varchar, day varchar, week varchar, month varchar, year varchar, weekday varchar)
+CREATE TABLE IF NOT EXISTS time(
+            id serial PRIMARY key,
+            start_time varchar  not null,
+            hour int,
+            day int,
+            week int, 
+            month int,
+            year int, 
+            weekday int)
+            ;
 """)
 
 songplay_table_create =  ("""
-CREATE TABLE IF NOT EXISTS songplays(start_time varchar , user_id varchar, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);
+CREATE TABLE IF NOT EXISTS songplays(
+            id serial primary key,
+            start_time varchar , 
+            user_id varchar, 
+            level varchar, 
+            song_id varchar,
+            artist_id varchar,
+            session_id int  not null, 
+            location varchar,
+            user_agent varchar)
+            ;
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = (""" 
-INSERT INTO songplays (start_time  , user_id , level , song_id , artist_id , session_id , location , user_agent ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s);
+INSERT INTO songplays (start_time  ,user_id , level ,song_id , artist_id ,session_id , location , user_agent) 
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+ON CONFLICT DO NOTHING;
 """) 
 
 user_table_insert = ("""
-INSERT INTO users (user_id, first_name, last_name , gender, level) VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT DO NOTHING;
+INSERT INTO users (user_id, first_name, last_name , gender, level) 
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) 
+DO
+    UPDATE SET level = users.level;
 """)
 
 song_table_insert = ("""
-INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES(%s, %s, %s, %s, %s);
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
 """)
 
 artist_table_insert = ("""
-INSERT INTO artists (artist_id, name , location ,latitude , longitude) VALUES (%s, %s, %s, %s, %s);
+INSERT INTO artists (artist_id, name , location ,latitude , longitude)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
 """)
 
 
 time_table_insert = ("""
-INSERT INTO time (start_time, hour , day , week , month , year , weekday) VALUES (%s , %s, %s, %s, %s, %s, %s);
+INSERT INTO time (start_time, hour , day , week , month , year , weekday) 
+VALUES (%s , %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
 """)
 
 # FIND SONGS
@@ -59,7 +107,7 @@ song_select = ("""
 SELECT s.song_id, a.artist_id
 FROM songs s
 INNER JOIN artists a ON s.artist_id = a.artist_id
-WHERE s.title = '%s' AND a.name = '%s' AND s.duration = '%s';
+WHERE s.title = (%s) AND a.name = (%s) AND s.duration = (%s);
 """)
 
 # QUERY LISTS
