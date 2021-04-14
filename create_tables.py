d78f059cf521efe285dddd348c93f6fd19c63a4a
Felipe Antonio Zadinello <felipezad@gmail.com>
2021-04-14 19:27:20 -0300

    Add files via upload

--- create_tables.py
+++ create_tables.py
@@ -1,37 +1,13 @@
+import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
-
-
-def create_database():
-    """
-    - Creates and connects to the sparkifydb
-    - Returns the connection and cursor to sparkifydb
-    """
-    
-    # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
-    conn.set_session(autocommit=True)
-    cur = conn.cursor()
-    
-    # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
-
-    # close connection to default database
-    conn.close()    
-    
-    # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
-    cur = conn.cursor()
-    
-    return cur, conn
+from botocore.exceptions import ClientError
+import pandas as pd
+import boto3
+import json
 
 
 def drop_tables(cur, conn):
-    """
-    Drops each table using the queries in `drop_table_queries` list.
-    """
-    
     for query in drop_table_queries:
         print(query)
         cur.execute(query)
@@ -39,30 +15,81 @@ def drop_tables(cur, conn):
 
 
 def create_tables(cur, conn):
-    """
-    Creates each table using the queries in `create_table_queries` list. 
-    """
     for query in create_table_queries:
-        print("my query is -> "+query)
         cur.execute(query)
         conn.commit()
+        
+# def createRole(KEY, SECRET, DB_IAM_ROLE_NAME):
+   
+#     iam = boto3.client('iam',aws_access_key_id=KEY,
+#                      aws_secret_access_key=SECRET,
+#                      region_name='us-west-2'
+#                   )
+#     #1.1 Create the role, 
+#     try:
+#         print("1.1 Creating a new IAM Role") 
+#         dwhRole = iam.create_role(
+#             Path='/',
+#             RoleName=DB_IAM_ROLE_NAME,
+#             Description = "Allows Redshift clusters to call AWS services on your behalf.",
+#             AssumeRolePolicyDocument=json.dumps(
+#                 {'Statement': [{'Action': 'sts:AssumeRole',
+#                    'Effect': 'Allow',
+#                    'Principal': {'Service': 'redshift.amazonaws.com'}}],
+#                  'Version': '2012-10-17'})
+#         )    
+#     except Exception as e:
+#         print(e)
 
+
+#     print("1.2 Attaching Policy")
+
+#     iam.attach_role_policy(RoleName=DB_IAM_ROLE_NAME,
+#                            PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+#                           )['ResponseMetadata']['HTTPStatusCode']
+
+#     print("1.3 Get the IAM role ARN")
+#     roleArn = iam.get_role(RoleName=DB_IAM_ROLE_NAME)['Role']['Arn']
+
+#     print(roleArn)
+
+    
+# def createCluster():
+#     try:
+#         response = redshift.create_cluster(        
+#         #HW
+#         ClusterType=DWH_CLUSTER_TYPE,
+#         NodeType=DWH_NODE_TYPE,
+#         NumberOfNodes=int(DWH_NUM_NODES),
+
+#         #Identifiers & Credentials
+#         DBName=DWH_DB,
+#         ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
+#         MasterUsername=DWH_DB_USER,
+#         MasterUserPassword=DWH_DB_PASSWORD,
+        
+#         #Roles (for s3 access)
+#         IamRoles=[roleArn]  
+#     )
+#     except Exception as e:
+#         print(e)
 
 def main():
-    """
-    - Drops (if exists) and Creates the sparkify database. 
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
     
-    - Establishes connection with the sparkify database and gets
-    cursor to it.  
+#     DB_IAM_ROLE_NAME       = config.get("IAM_ROLE", "DB_IAM_ROLE_NAME")  
+#     KEY                    = config.get('AWS','KEY')
+#     SECRET                 = config.get('AWS','SECRET')
+#     createRole(KEY,SECRET,DB_IAM_ROLE_NAME)
     
-    - Drops all the tables.  
     
-    - Creates all tables needed. 
+    url = "host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values())
+    print(url)
     
-    - Finally, closes the connection. 
-    """
-    cur, conn = create_database()
-    
+    conn = psycopg2.connect(url)
+    cur = conn.cursor()
+
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
